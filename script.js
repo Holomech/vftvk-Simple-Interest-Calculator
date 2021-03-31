62f090a8ef096deeff88cfe02e37a00ee1a8d31f
Holomech <vozendan@gmail.com>
2021-03-31 21:56:51 +0200

    Corrected result computation

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function compute()
 {
     let principal = parseInt(document.getElementById("principal").value);
-    let rate = parseInt(document.getElementById("rate").value);
+    let rate = parseFloat(document.getElementById("rate").value);
     let years = parseInt(document.getElementById("years").value);
     let interest = principal * years * rate / 100;
     let end_year = new Date().getFullYear() + years;
-    let end_sum = interest + principal;
     let result_text = "If you deposit <mark>"+principal +"</mark>,"+"<br>at an interest rate of <mark>"+rate+
-        "%</mark>.<br>You will receive an amount of <mark>"+end_sum+
+        "%</mark>.<br>You will receive an amount of <mark>"+interest+
         "</mark>,<br>in the year <mark>"+end_year+"</mark>.";
 
     document.getElementById("result").innerHTML = result_text;
