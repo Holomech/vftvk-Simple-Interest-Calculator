781aec74b5e53d82c25702519b9a4224b94fa8f9
Holomech <vozendan@gmail.com>
2021-03-31 21:48:30 +0200

    Written the basic functionality

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function compute()
 {
-    let principal = document.getElementById("principal").value;
-    let rate = document.getElementById("rate").value;
-    let years = document.getElementById("years").value;
+    let principal = parseInt(document.getElementById("principal").value);
+    let rate = parseInt(document.getElementById("rate").value);
+    let years = parseInt(document.getElementById("years").value);
     let interest = principal * years * rate / 100;
+    let end_year = new Date().getFullYear() + years;
+    let end_sum = interest + principal;
+    let result_text = "If you deposit <mark>"+principal +"</mark>,"+"<br>at an interest rate of <mark>"+rate+
+        "%</mark>.<br>You will receive an amount of <mark>"+end_sum+
+        "</mark>,<br>in the year <mark>"+end_year+"</mark>.";
+
+    document.getElementById("result").innerHTML = result_text;
+}
+
+function displayValue()
+{
+    let range_value = document.getElementById("rate").value;
+    document.getElementById("current_rate").textContent = range_value + "%";
 }
