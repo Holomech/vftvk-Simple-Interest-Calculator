d9f1120570b4e2b00f4e381178ac246265909672
Holomech <vozendan@gmail.com>
2021-04-01 11:20:13 +0200

    Added form validation to compute function

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function compute()
 {
     let principal = parseInt(document.getElementById("principal").value);
-    let rate = parseFloat(document.getElementById("rate").value);
-    let years = parseInt(document.getElementById("years").value);
-    let interest = principal * years * rate / 100;
-    let end_year = new Date().getFullYear() + years;
-    let result_text = "If you deposit <mark>"+principal +"</mark>,"+"<br>at an interest rate of <mark>"+rate+
-        "%</mark>.<br>You will receive an amount of <mark>"+interest+
-        "</mark>,<br>in the year <mark>"+end_year+"</mark>.";
+    if (principal <= 0)
+    {
+        alert("Enter a positive number as Amount.");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+        let rate = parseFloat(document.getElementById("rate").value);
+        let years = parseInt(document.getElementById("years").value);
+        let interest = principal * years * rate / 100;
+        let end_year = new Date().getFullYear() + years;
+        let result_text = "If you deposit <mark>"+principal +"</mark>,"+"<br>at an interest rate of <mark>"+rate+
+            "%</mark>.<br>You will receive an amount of <mark>"+interest+
+            "</mark>,<br>in the year <mark>"+end_year+"</mark>.";
 
-    document.getElementById("result").innerHTML = result_text;
+        document.getElementById("result").innerHTML = result_text;
+    }
 }
 
 function displayValue()
